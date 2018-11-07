e88199d9dcefb67b8778e693c39ee76b075e35bb
Mondotrasho <oscar@peoplepictures.com.au>
2018-11-07 20:11:33 +1100

    Basic terminal LIKE app

--- RPC_Client/Program.cs
+++ RPC_Client/Program.cs
@@ -1,17 +1,90 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Remoting.Channels;
 
-namespace RPC_Client
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        //Create Client
+        TcpClientChannel channel = new TcpClientChannel();
+        ChannelServices.RegisterChannel(channel, false);
+
+        //Get a reference to the player on the server
+        string playerURL = "tcp://localhost:" + 1337 + "/" + "Little";
+        Player player = (Player)Activator.GetObject(typeof(Player), playerURL);
+
+        while (true)
         {
+            Console.Write("Type a message to the server or type 'quit' to exit\n");
+            string text = Console.ReadLine();
+            if (text == "quit")
+                break;
+            //RPC: Call function on server
+            
 
+            //extended functionalityVV
 
+            if (text == "help" || text == "h" || text == "?" || text == "-h" || text == "--h" || text == "-help" || text == "--help")
+            {
+                //list commands
+                //ls
+                //login
+                //quit
+                string[] lsResults = new[] {
+                    "----------------------------",
+                    "-                          -",
+                    "-         COMMANDS         -",
+                    "-          -----           -",
+                    "-         /ls              -",
+                    "-         /login           -",
+                    "-         /quit            -",
+                    "-         /                -",
+                    "-                          -",
+                    "----------------------------" };
+
+                player.SayHello(lsResults);
+                
+            }
+
+            if (text == "ls")
+            {
+                //list filesystem
+                string[] fileSystem = new[] {
+                    "----------------------------",
+                    "-                          -",
+                    "-          FILES           -",
+                    "-          -----           -",
+                    "-         DOCUMENTS        -",
+                    "-         PICTURES         -",
+                    "-         MOVIES           -",
+                    "-         WORK             -",
+                    "-                          -",
+                    "----------------------------" };
+
+                player.SayHello(fileSystem);
+                
+            }
+
+            if (text == "login")
+            {
+                //list filesystem
+                string[] loginUI = new[] {
+                    "----------------------------",
+                    "-          LOGIN           -",
+                    "-                          -",
+                    "-          USER            -",
+                    "-          ADMIN_          -",
+                    "-                          -",
+                    "-          PASS            -",
+                    "-          ******_         -",
+                    "-                          -",
+                    "----------------------------" };
+
+                player.SayHello(loginUI);
+                
+            }
         }
     }
 }
